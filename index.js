6282d7ab6489f55385644cbfd6ed3f685ff582e0
sohagislampk <sohagislampk@gmail.com>
2022-11-09 11:18:14 +0600

    Single package by id API created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -23,6 +23,12 @@ async function run() {
             const cursor = packagesCollection.find(query);
             const packages = await cursor.toArray();
             res.send(packages);
+        });
+        app.get('/packages/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tourPackage = await packagesCollection.findOne(query);
+            res.send(tourPackage);
         })
     }
     finally {
